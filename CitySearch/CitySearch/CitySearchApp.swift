08478a3516d8efcffc47ed20a06e52c01fbcecd1
Damian Modernell <damian.modernell@gmail.com>
2025-08-31 15:11:41 -0300

    Created initial app in xcode and its boilerplate code

---
+++ CitySearch/CitySearch/CitySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySearchApp.swift
+//  CitySearch
+//
+//  Created by Damian Modernell on 31/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CitySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySearch/CitySearchTests/CitySearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  CitySearchTests.swift
+//  CitySearchTests
+//
+//  Created by Damian Modernell on 31/8/25.
+//
+
+import Testing
+@testable import CitySearch
+
+struct CitySearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
